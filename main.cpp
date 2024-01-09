518c8d9627e66f831808e7bdb90fe9cd60dfa2fc
Reinhold Sauerbrey <reinhold.sauerbrey@bs-erlangen.de>
2024-01-09 12:12:47 +0100

    Class Raumschiff

--- main.cpp
+++ main.cpp
@@ -6,11 +6,41 @@ class Raumschiff
 {
 private:
     string Name;
+    int Energie;
 public:
+    void setName(string Name);
+    string getName();
+    void setEnergie(int Energie);
+    int getEnergie();
 };
+
+void Raumschiff::setName(string Name)
+{
+    this->Name = Name;
+}
+
+string Raumschiff::getName()
+{
+    return Name;
+}
+
+void Raumschiff::setEnergie(int Energie)
+{
+    this->Energie = Energie;
+}
+
+int Raumschiff::getEnergie()
+{
+    return Energie;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Raumschiff r;
+    r.setName("Enterprise");
+    r.setEnergie(77777);
+    cout << r.getName() << endl;
+    cout << r.getEnergie() << endl;
+
     return 0;
 }
